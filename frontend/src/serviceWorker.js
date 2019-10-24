82786c2b1e22c2b529e7668fa83f4c9a463f0033
RXWE <rxwe5607@snu.ac.kr>
2019-10-24 20:26:30 +0900

    travis-tutorial

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -11,8 +11,8 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
+  window.location.hostname === 'localhost' || 
+  // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
