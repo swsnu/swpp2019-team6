668e9cc9df1a9a5441a6a65218ae8ccca823a157
RXWE <rxwe5607@snu.ac.kr>
2019-10-24 20:20:25 +0900

    minor change for testing travis

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store, { history } from "./store/store";
 
+// render
 ReactDOM.render(
   <Provider store={store}>
     <App history={history} />
