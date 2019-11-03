23907ff1db534e3edcee938f86f738abb5616ae6
dreamsh19 <dreamsh19@snu.ac.kr>
2019-11-03 22:14:37 +0900

    Implement test for SignupPage and userReducer

---
+++ frontend/src/pages/SignupPage.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import SignupPage from './SignupPage'
+
+
+describe('<SignupPage />', () => {
+    it('should render SignupPage without errors', () => {
+        const component = shallow(<SignupPage />)
+        const wrapper= component.find('.SignupPage')
+        expect(wrapper.length).toBe(1)
+    })
+    
+}
+)
