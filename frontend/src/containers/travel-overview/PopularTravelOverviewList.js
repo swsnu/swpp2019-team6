222be7e022571b7297323b7b6d4d5d5e650d4834
RXWE <rxwe5607@snu.ac.kr>
2019-11-06 02:20:32 +0900

    added some data

--- frontend/src/containers/travel-overview/PopularTravelOverviewList.js
+++ frontend/src/containers/travel-overview/PopularTravelOverviewList.js
@@ -6,25 +6,25 @@ import TravelOverviewList from '../../components/travel-overview/TravelOverviewL
 class PopularTravelOverviewList extends Component {
     state = {
       travelList: [{
-        title: 'Seoul Palace Tour',
+        title: 'Seoul City Tour',
         author: 'Alice',
-        summary: 'Exciting time warp to Chosun Dyasty. Feel the vivid color of Korea.',
+        summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
         period: '2019.10.01 ~ 2019.10.04',
         likes: 57,
         photo: '/images/1.jpeg',
       },
       {
-        title: 'New York Tour',
+        title: 'New York, it\'s NEW YORK!',
         author: 'Bob',
-        summary: 'IT\'S NEW YORK.',
-        period: '2019.01.03 ~ 2019.10.15',
+        summary: 'Felis donec et odio pellentesque diam volutpat.',
+        period: '2019.02.03 ~ 2019.02.15',
         likes: 30,
         photo: '/images/2.jpeg',
       },
       {
-        title: 'Fantasy in Florence',
+        title: '3 days in Ha Noi',
         author: 'Catherine',
-        summary: 'Welcome!',
+        summary: 'Non pulvinar neque laoreet suspendisse interdum consectetur.',
         period: '2019.07.29 ~ 2019.08.01',
         likes: 19,
         photo: '/images/3.jpeg',
