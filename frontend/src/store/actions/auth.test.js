a6fc8e746bcfbec33f2953e41ccf5e9c3bcd9522
soon <dyd3312@gmail.com>
2019-11-28 15:48:12 +0900

    change test order

--- frontend/src/store/actions/auth.test.js
+++ frontend/src/store/actions/auth.test.js
@@ -12,46 +12,15 @@ const stubUser = {
 };
 
 describe('ActionCreators : login', () => {
+  let spyAxiosPost;
   afterEach(() => {
     jest.clearAllMocks();
-    const spyAlert = jest.spyOn(window, 'alert').mockImplementation(() => { return () => {}; });
-  });
-
-  it('\'loginRequest\' should change login status and user status correctly', (done) => {
-    const stubLoginInfo = { email: 'test1@test.io', password: 'test' };
-
-    const spy = jest.spyOn(axios, 'post')
-      .mockImplementation((url, user) => {
-        return new Promise((resolve, reject) => {
-          const result = {
-            status: 200,
-            data: {
-              token: '123',
-              user: stubUser,
-            },
-          };
-          const result_fail = {
-            status: 400,
-            data: 'test',
-          };
-          resolve(result);
-          reject(result_fail);
-        });
-      });
-
-    store.dispatch(actionCreators.login(stubLoginInfo)).then(() => {
-      const newState = store.getState();
-      expect(newState.auth.user).toEqual(stubUser);
-      expect(newState.auth.token).toEqual('123');
-      expect(spy).toHaveBeenCalledTimes(1);
-      done();
-    });
   });
 
   it('\'loginRequest\' Fail', (done) => {
     const stubLoginInfo = { email: 'wrong@wrong.io', password: 'wrong' };
 
-    const spy = jest.spyOn(axios, 'post')
+    spyAxiosPost = jest.spyOn(axios, 'post')
       .mockImplementation((url, user) => {
         return new Promise((resolve, reject) => {
           const result = {
@@ -69,10 +38,37 @@ describe('ActionCreators : login', () => {
       expect(newState.auth.user).toBeFalsy();
       expect(newState.auth.token).toBeFalsy();
       expect(spyAlert).toBeCalled();
-      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spyAxiosPost).toHaveBeenCalledTimes(1);
       done();
     });
   });
+
+  it('\'loginRequest\' should change login status and user status correctly', (done) => {
+    const stubLoginInfo = { email: 'test1@test.io', password: 'test' };
+
+    spyAxiosPost = jest.spyOn(axios, 'post')
+      .mockImplementation((url, user) => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: {
+              token: '123',
+              user: stubUser,
+            },
+          };
+          resolve(result);
+        });
+      });
+
+    store.dispatch(actionCreators.login(stubLoginInfo)).then(() => {
+      const newState = store.getState();
+      expect(newState.auth.user).toEqual(stubUser);
+      expect(newState.auth.token).toEqual('123');
+      expect(spyAxiosPost).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
   it('\'logoutRequest\' should change login status and user status correctly', (done) => {
     store.dispatch(actionCreators.logout());
     const newState = store.getState();
