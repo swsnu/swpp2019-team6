b2985218a78a6a94c27c5b56363014f9be2f64fa
soon <dyd3312@gmail.com>
2019-11-29 08:49:00 +0900

    App.js test case

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,8 +6,40 @@ import App from './App';
 import { getMockStore } from './test-utils/mocks';
 import { history } from './store/store';
 
+
 const mockStore = getMockStore({
   state: 'empty',
+});
+
+jest.mock('./pages/LoginPage', () => {
+  return jest.fn((props) => {
+    return (<div className="loginPage" />);
+  });
+});
+jest.mock('./pages/MainPage', () => {
+  return jest.fn((props) => {
+    return (<div className="mainPage" />);
+  });
+});
+jest.mock('./pages/SignupPage', () => {
+  return jest.fn((props) => {
+    return (<div className="signupPage" />);
+  });
+});
+jest.mock('./pages/CreateTravel', () => {
+  return jest.fn((props) => {
+    return (<div className="createTravel" />);
+  });
+});
+jest.mock('./pages/SearchPage', () => {
+  return jest.fn((props) => {
+    return (<div className="searchPage" />);
+  });
+});
+jest.mock('./pages/UserInfoPage', () => {
+  return jest.fn((props) => {
+    return (<div className="UserInfoPage" />);
+  });
 });
 
 describe('App', () => {
@@ -24,5 +56,53 @@ describe('App', () => {
   it('should render.', () => {
     const component = mount(app);
     expect(component.find('.App').length).toBe(1);
+  });
+
+  it('should render render login', () => {
+    history.push('/login');
+    const component = mount(app);
+    expect(component.find('.loginPage').length).toBe(1);
+  });
+
+  it('should render render default', () => {
+    history.push('/');
+    const component = mount(app);
+    expect(component.find('.loginPage').length).toBe(1);
+  });
+
+  it('should render render signup', () => {
+    history.push('/sign_up');
+    const component = mount(app);
+    expect(component.find('.signupPage').length).toBe(1);
+  });
+
+  it('should render render signup', () => {
+    history.push('/main');
+    const component = mount(app);
+    expect(component.find('.mainPage').length).toBe(1);
+  });
+
+  it('should render render create travel', () => {
+    history.push('/travel/create');
+    const component = mount(app);
+    expect(component.find('.createTravel').length).toBe(1);
+  });
+
+  it('should render render search', () => {
+    history.push('/search');
+    const component = mount(app);
+    expect(component.find('.searchPage').length).toBe(1);
+  });
+
+  it('should render render user info', () => {
+    history.push('/user/id');
+    const component = mount(app);
+    expect(component.find('.UserInfoPage').length).toBe(1);
+  });
+
+  it('should render render error', () => {
+    history.push('/error');
+    const component = mount(app);
+    expect(component.find('#error').length).toBe(1);
   });
 });
