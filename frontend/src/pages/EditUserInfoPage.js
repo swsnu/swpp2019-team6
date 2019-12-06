a2d88f7f7de3afcdb3e84cf703d726ad52391bab
RXWE <rxwe5607@snu.ac.kr>
2019-12-06 17:04:43 +0900

    access for only my EditUserInfoPage

--- frontend/src/pages/EditUserInfoPage.js
+++ frontend/src/pages/EditUserInfoPage.js
@@ -3,12 +3,34 @@ import HeaderContainer from '../containers/common/HeaderContainer';
 import EditUserInfoContainer from '../containers/user-info-edit/EditUserInfoContainer';
 
 class EditUserInfoPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: JSON.parse(localStorage.getItem('user')),
+      is_mypage: false,
+    };
+  }
+
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      ...prevState,
+      is_mypage: (prevState.currentUser.id === parseInt(nextProps.match.params.id, 10)),
+    };
+  }
+
   render() {
     return (
-      <div className="editUserInfoPage">
-        <HeaderContainer />
-        <EditUserInfoContainer id={this.props.match.params.id} />
-      </div>
+      <>
+        {this.state.currentUser && this.state.is_mypage ? (
+          <div className="editUserInfoPage">
+            <HeaderContainer />
+            <EditUserInfoContainer id={this.props.match.params.id} />
+          </div>
+        ) : (
+          <span>{this.props.history.push('/error')}</span>
+        )}
+      </>
     );
   }
 }
