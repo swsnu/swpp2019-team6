a0cf12bd3027e7d3091105bcb3610e238be486a9
soon <dyd3312@gmail.com>
2019-12-06 14:15:09 +0900

    travel reducer test case

--- frontend/src/store/actions/travel.test.js
+++ frontend/src/store/actions/travel.test.js
@@ -108,6 +108,46 @@ const stubRequestTravel = {
         datetime: new Date(),
       },
     },
+    {
+      id: 'custom-0',
+      info: {
+        title: 'TEST',
+        startTime: new Date(),
+        startPoint: 'TEST',
+      },
+    },
+    {
+      id: 'activity-0',
+      info: {
+        title: 'TEST',
+        startTime: new Date(),
+        startPoint: 'TEST',
+      },
+    },
+    {
+      id: 'restaurant-0',
+      info: {
+        title: 'TEST',
+        startTime: new Date(),
+        startPoint: 'TEST',
+      },
+    },
+    {
+      id: 'hotel-0',
+      info: {
+        title: 'TEST',
+        startTime: new Date(),
+        startPoint: 'TEST',
+      },
+    },
+    {
+      id: 'transportation-0',
+      info: {
+        title: 'TEST',
+        startTime: new Date(),
+        startPoint: 'TEST',
+      },
+    },
   ],
 };
 
@@ -180,6 +220,42 @@ describe('Travel Action', () => {
       });
 
     store.dispatch(actionCreators.getTravel(999)).then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('should get popular travel info', (done) => {
+    const spy = jest.spyOn(axios, 'get')
+      .mockImplementation((url) => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: stubTravel,
+          };
+          resolve(result);
+        });
+      });
+
+    store.dispatch(actionCreators.getPopularTravels()).then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('should get recent travel info', (done) => {
+    const spy = jest.spyOn(axios, 'get')
+      .mockImplementation((url) => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: stubTravel,
+          };
+          resolve(result);
+        });
+      });
+
+    store.dispatch(actionCreators.getRecentTravels()).then(() => {
       expect(spy).toHaveBeenCalledTimes(1);
       done();
     });
