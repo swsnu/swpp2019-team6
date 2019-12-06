bfd3fa2b6969e1907e4ac1f04e2fff4bde806745
soon <dyd3312@gmail.com>
2019-12-06 10:04:13 +0900

    travel overview test case

--- frontend/src/containers/travel-overview/SearchTravelOverviewList.test.js
+++ frontend/src/containers/travel-overview/SearchTravelOverviewList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Typography from '@material-ui/core/Typography';
 import SearchTravelOverviewList from './SearchTravelOverviewList';
 
