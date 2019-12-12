01acb5f1f81e722a86d94e3ed13917a54a7041a5
Jangdonghae <jangdonghae@gmail.com>
2019-12-12 22:58:49 +0900

    code for sentence embedding

--- backend/triplannet/travel/travelembed.py
+++ backend/triplannet/travel/travelembed.py
@@ -2,12 +2,16 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
 import pandas as pd
+import os
 
 #tf.compat.v1.disable_eager_execution()
 
 
 def travel_text_embed_vector(text):
-    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    module_url = "/home/jdh/swpp-team6-project/swpp2019-team6/backend/triplannet/travel/encoder"
+    #module_url = "./encoder"
     embed=hub.load(module_url)
     travel_embed=embed([text])
-    return np.array(travel_embed)[0]
+    travel_embed=100000*np.array(travel_embed)[0]
+    travel_embed=travel_embed.astype(int)
+    return list(travel_embed)
