74966715ac43801a2e098354e41f78402b918a50
RXWE <rxwe5607@snu.ac.kr>
2019-12-17 20:32:23 +0900

    deleted temp data

--- frontend/src/containers/travel-overview/PopularTravelOverviewList.js
+++ frontend/src/containers/travel-overview/PopularTravelOverviewList.js
@@ -6,31 +6,6 @@ import TravelOverviewList from '../../components/travel-overview/TravelOverviewL
 
 import * as actionCreators from '../../store/actions/index';
 
-const tempTravelList = [{
-  title: 'Pharetra diam sit amet nisl',
-  author: 'Alice',
-  summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
-  period: '2019.10.01 ~ 2019.10.04',
-  likes: 57,
-  photo: '/images/1.jpeg',
-},
-{
-  title: 'Imperdiet dui accumsan sit amet nulla facilisi',
-  author: 'Bob',
-  summary: 'Sagittis orci a scelerisque purus semper eget. Sed felis eget velit aliquet sagittis id consectetur. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales.',
-  period: '2019.02.03 ~ 2019.02.15',
-  likes: 30,
-  photo: '/images/2.jpeg',
-},
-{
-  title: 'Arcu dui vivamus arcu felis bibendum ut',
-  author: 'Catherine',
-  summary: 'Sed ullamcorper morbi tincidunt ornare massa. Velit euismod in pellentesque massa placerat duis ultricies lacus.',
-  period: '2019.07.29 ~ 2019.08.01',
-  likes: 19,
-  photo: '/images/3.jpeg',
-}];
-
 class PopularTravelOverviewList extends Component {
   componentDidMount() {
     this.props.onGetPopularTravels();
--- frontend/src/containers/travel-overview/RecentTravelOverviewList.js
+++ frontend/src/containers/travel-overview/RecentTravelOverviewList.js
@@ -6,55 +6,6 @@ import TravelOverviewList from '../../components/travel-overview/TravelOverviewL
 
 import * as actionCreators from '../../store/actions/index';
 
-const tempTravelList = [{
-  title: 'Ante metus dictum at tempor commodo ullamcorper a lacus',
-  author: 'Jason',
-  summary: 'Fringilla ut morbi tincidunt augue interdum velit euismod.',
-  period: '2019.02.04 ~ 2019.02.06',
-  likes: 41,
-  photo: '/images/5.jpeg',
-},
-{
-  title: 'Eros in cursus turpis massa tincidunt',
-  author: 'Kacey',
-  summary: 'Elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Id venenatis a condimentum vitae sapien pellentesque habitant.',
-  period: '2019.11.03 ~ 2019.11.10',
-  likes: 28,
-  photo: null,
-},
-{
-  title: 'Euismod quis viverra nibh cras',
-  author: 'Lucy',
-  summary: 'Sem nulla pharetra diam sit amet nisl suscipit. Lacus vestibulum sed arcu non.',
-  period: '2019.12.23 ~ 2019.12.25',
-  likes: 24,
-  photo: '/images/7.jpeg',
-},
-{
-  title: 'Mus mauris vitae ultricies leo integer',
-  author: 'Matthew',
-  summary: 'Eu sem integer vitae justo eget magna fermentum iaculis.',
-  period: '2019.05.03 ~ 2019.05.05',
-  likes: 15,
-  photo: '/images/9.jpeg',
-},
-{
-  title: 'Turpis tincidunt id aliquet risus. Congue eu consequat ac felis',
-  author: 'Nicolas',
-  summary: 'Ultricies tristique nulla aliquet enim tortor at. Tortor pretium viverra suspendisse potenti nullam.',
-  period: '2019.07.25 ~ 2019.08.01',
-  likes: 9,
-  photo: '/images/10.jpeg',
-},
-{
-  title: 'Ridiculus mus mauris vitae',
-  author: 'Olivia',
-  summary: 'Ac tortor vitae purus faucibus. Odio eu feugiat pretium nibh ipsumi.',
-  period: '2019.07.29 ~ 2019.08.01',
-  likes: 6,
-  photo: null,
-}];
-
 class RecentTravelOverviewList extends Component {
   componentDidMount() {
     this.props.onGetRecentTravels();
